343771a450a51acf937db1b065b8527aeac7b9f0
Florian Rivière <102306011+FlorianRiviere@users.noreply.github.com>
2022-07-21 15:36:26 +0200

    Paramétrage des controllers

--- back/controllers/user.js
+++ back/controllers/user.js
@@ -3,6 +3,45 @@ const jwt = require("jsonwebtoken");
 
 const User = require("../models/user");
 
-exports.signup = (req.res.next) => {};
+exports.signup = (req, res, next) => {
+  bcrypt
+    .hash(req.body.password, 10)
+    .then((hash) => {
+      const user = new User({
+        email: req.body.email,
+        password: hash,
+      });
 
-exports.login = (req.res.next) => {};
+      user
+        .save()
+        .then(() => res.status(201).json({ message: "User account created" }))
+        .catch(() => res.status(400).json({ error }));
+    })
+    .catch(() => res.status(500).json({ error }));
+};
+
+exports.login = (req, res, next) => {
+  User.findOne({
+    email: req.body.email,
+  })
+    .then((user) => {
+      if (!user) {
+        return res.status(401).json({ error: "User not found!" });
+      }
+      bcrypt
+        .compare(req.body.password, user.password)
+        .then((valid) => {
+          if (!valid) {
+            return res.status(401).json({ error: "Wrong password!" });
+          }
+          res.status(200).json({
+            userId: user_id,
+            token: jwt.sign({ userId: user._id }, "RANDOM_TOKEN_SECRET", {
+              expiresIIn: "24h",
+            }),
+          });
+        })
+        .catch((error) => res.status(501).json({ error }));
+    })
+    .catch((error) => res.status(500).json({ error }));
+};
