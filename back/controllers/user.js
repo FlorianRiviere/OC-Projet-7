d51ad4f9c19325da69fe0ab953142211dd01803b
Florian Rivière <102306011+FlorianRiviere@users.noreply.github.com>
2022-07-22 16:10:56 +0200

    Ajout de CRUD user supplémentaires, ajout dossier images

--- back/controllers/user.js
+++ back/controllers/user.js
@@ -12,6 +12,7 @@ exports.signup = (req, res, next) => {
         password: hash,
         firstName: req.body.firstName,
         lastName: req.body.lastName,
+        department: req.body.department,
       });
 
       user
@@ -38,12 +39,43 @@ exports.login = (req, res, next) => {
           }
           res.status(200).json({
             userId: user_id,
-            token: jwt.sign({ userId: user._id }, "RANDOM_TOKEN_SECRET", {
-              expiresIn: "24h",
-            }),
+            token: jwt.sign(
+              { userId: user._id },
+              "RANDOM_TOKEN_SECRET",
+              {
+                expiresIn: "24h",
+              },
+              res.status(201).json({ message: "User connected" })
+            ),
           });
         })
         .catch((error) => res.status(501).json({ error }));
     })
     .catch((error) => res.status(500).json({ error }));
+};
+
+exports.getAllUsers = (req, res, next) => {
+  User.find()
+    .select("-password")
+    .then((users) => res.status(200).json(users))
+    .catch((error) => res.status(401).json(error));
+};
+
+exports.getUser = (req, res, next) => {
+  User.findOne({ _id: req.params.id })
+    .select("-password")
+    .then((users) => res.status(200).json(users))
+    .catch((error) => res.status(401).json(error));
+};
+
+exports.updateUser = (req, res, next) => {
+  User.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+    .then(() => res.status(200).json({ message: "User modified" }))
+    .catch((error) => res.status(400).json(error));
+};
+
+exports.deleteUser = (req, res, next) => {
+  User.deleteOne({ _id: req.params.id })
+    .then(() => res.status(200).json({ message: "User deleted" }))
+    .catch((error) => res.status(400).json(error));
 };
--- back/routes/user.js
+++ back/routes/user.js
@@ -7,4 +7,9 @@ const userCtrl = require("../controllers/user");
 router.post("/signup", password, userCtrl.signup);
 router.post("/login", userCtrl.login);
 
+router.get("/", userCtrl.getAllUsers);
+router.get("/:id", userCtrl.getUser);
+router.put("/:id", userCtrl.updateUser);
+router.delete("/:id", userCtrl.deleteUser);
+
 module.exports = router;
