e5685b26e39e4c50e0079ac5a23996ac274812e7
Florian Rivière <102306011+FlorianRiviere@users.noreply.github.com>
2022-07-28 13:34:13 +0200

    résoudre problèmes

--- front/src/pages/Home.js
+++ front/src/pages/Home.js
@@ -1,13 +1,7 @@
 import React from "react";
 
-<<<<<<< HEAD
-const Home = () => {
-  return <div>Bienvenue ! </div>;
-};
-=======
 function Home() {
   return <div>Bienvenue !</div>;
 }
->>>>>>> front
 
 export default Home;
--- front/src/pages/Profil.js
+++ front/src/pages/Profil.js
@@ -1,11 +1,7 @@
 import React from "react";
 
 function Profil() {
-<<<<<<< HEAD
-  return <div>Profil ! </div>;
-=======
   return <div>Profil !</div>;
->>>>>>> front
 }
 
 export default Profil;
