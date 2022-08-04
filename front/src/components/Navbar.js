b2bdde6efe5ba32497570578408067922a8d7bd4
Florian Rivière <102306011+FlorianRiviere@users.noreply.github.com>
2022-08-04 10:55:44 +0200

    Ajout de l'image de profil dans la navbar

--- front/src/components/Navbar.js
+++ front/src/components/Navbar.js
@@ -1,18 +1,43 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
+import axios from "axios";
 import { NavLink } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { getUserData } from "../feature/userSlice";
+import { UidContext } from "../components/AppContext";
 import Logout from "./Log/Logout";
 import HouseUser from "../assets/icons/house-user-solid.svg";
 import House from "../assets/icons/house-solid.svg";
 import Group from "../assets/icons/user-group-solid.svg";
 
 function Navbar() {
+  const uid = useContext(UidContext);
+
+  const [loadUser, setLoadUser] = useState(true);
+  const dispatch = useDispatch();
+  const userData = useSelector((state) => state.user.user);
+
+  useEffect(() => {
+    if (loadUser) {
+      axios({
+        method: "get",
+        url: `${process.env.REACT_APP_API_URL}api/users/${uid}`,
+        withCredentials: true,
+      })
+        .then((res) => {
+          dispatch(getUserData(res.data));
+          setLoadUser(false);
+        })
+        .catch((err) => console.log(err));
+    }
+  }, [dispatch, uid, loadUser]);
+
   return (
     <>
       <nav>
         <div className="navbar">
           <NavLink className="navlink" to="/profil">
             <img
-              src={HouseUser}
+              src={userData.picture}
               alt="Icone d'une maison avec une personne dedans"
               title="Profil"
             />
