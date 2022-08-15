dc125264c96381d6d13096f0e7fb32f01afe76d5
Florian Rivière <102306011+FlorianRiviere@users.noreply.github.com>
2022-08-15 14:56:21 +0200

    Modfication de la card new post

--- front/src/components/Routes/index.js
+++ front/src/components/Routes/index.js
@@ -9,31 +9,26 @@ import Home from "../../pages/Home";
 import Profil from "../../pages/Profil";
 import Group from "../../pages/Group";
 import { UidContext } from "../AppContext";
-import { useSelector } from "react-redux";
 
 function Index() {
   const uid = useContext(UidContext);
 
-  const userData = useSelector((state) => state.user.user);
-
-  if (userData !== null) {
-    return (
-      <>
-        <Router>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/profil/:id" element={<Profil />} />
-            <Route
-              path="/profil"
-              element={<Navigate to={`/profil/${uid}`} replace />}
-            />
-            <Route path="/group" element={<Group />} />
-            <Route path="*" element={<Navigate to="/" replace />} />
-          </Routes>
-        </Router>
-      </>
-    );
-  }
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profil/:id" element={<Profil />} />
+          <Route
+            path="/profil"
+            element={<Navigate to={`/profil/${uid}`} replace />}
+          />
+          <Route path="/group" element={<Group />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default Index;
